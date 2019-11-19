0d14bf2fdfb99b299277051498a9ccb753efbc6f
Megha <megha.maheshwari@10to8.com>
2019-11-19 17:04:49 +0000

    Fix for urls property in api.py

--- tastypie/api.py
+++ tastypie/api.py
@@ -100,7 +100,7 @@ class Api(object):
 
         for name in sorted(self._registry.keys()):
             self._registry[name].api_name = self.api_name
-            pattern_list.append((r"^(?P<api_name>%s)/" % self.api_name, include(self._registry[name].urls)))
+            pattern_list.append((url(r"^(?P<api_name>%s)/" % self.api_name, include(self._registry[name].urls))))
 
         urlpatterns = self.prepend_urls()
 
@@ -108,10 +108,10 @@ class Api(object):
             #warnings.warn("'override_urls' is a deprecated method & will be removed by v1.0.0. Please rename your method to ``prepend_urls``.")
             urlpatterns += self.override_urls()
 
-        urlpatterns += [
-            pattern_list
-        ]
+        urlpatterns += pattern_list
+
         return urlpatterns
+
 
     def top_level(self, request, api_name=None):
         """
