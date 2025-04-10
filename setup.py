297d4a8413bbf9ec7775d06772672c98f61465da
Pawan Kumar <86656006+pawankth@users.noreply.github.com>
2025-04-10 13:59:03 +0530

    bumping version from 0.9.12-alpha to 0.9.13-alpha

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-tastypie',
-    version='0.9.12-alpha',
+    version='0.9.13-alpha',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
